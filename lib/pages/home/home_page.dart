bacafd1426a700f0c376b6358ead1d5e1e1200a2
Rayhan Mahardhika Wijaya <rayhan@kitalulus.com>
2022-08-06 00:29:46 +0700

    feat: done home page slicing

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/widgets/product_card.dart';
+import 'package:shamo/pages/widgets/product_tile.dart';
 import 'package:shamo/theme.dart';
 
 class HomePage extends StatelessWidget {
@@ -131,10 +133,72 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  Widget popularProductsTitle() {
+    return Container(
+      margin: EdgeInsets.only(
+          top: defaultMargin, left: defaultMargin, right: defaultMargin),
+      child: Text(
+        'Popular Products',
+        style: primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+      ),
+    );
+  }
+
+  Widget popularProducts() {
+    return Container(
+      margin: EdgeInsets.only(top: 14),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            SizedBox(
+              width: defaultMargin,
+            ),
+            Row(
+              children: [ProductCard(), ProductCard(), ProductCard()],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget newArrivalsTitle() {
+    return Container(
+      margin: EdgeInsets.only(
+          top: defaultMargin, left: defaultMargin, right: defaultMargin),
+      child: Text(
+        'New Arrivals',
+        style: primaryTextStyle.copyWith(fontSize: 22, fontWeight: semiBold),
+      ),
+    );
+  }
+
+  Widget newArrivals() {
+    return Container(
+      margin: EdgeInsets.only(top: 14),
+      child: Column(
+        children: [
+          ProductTile(),
+          ProductTile(),
+          ProductTile(),
+          ProductTile(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: [header(), categories()],
+      children: [
+        header(),
+        categories(),
+        popularProductsTitle(),
+        popularProducts(),
+        newArrivalsTitle(),
+        newArrivals()
+      ],
     );
   }
 }
