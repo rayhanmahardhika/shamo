2092371340d76e258ec5e0d1610951da2f8f775a
Rayhan Mahardhika Wijaya <rayhan@kitalulus.com>
2022-08-06 22:26:22 +0700

    feat: chat page done

--- lib/theme.dart
+++ lib/theme.dart
@@ -11,10 +11,11 @@ Color priceColor = Color(0xff2C96F1);
 Color backgroundColor1 = Color(0xff1F1D2B);
 Color backgroundColor2 = Color(0xff2B2937);
 Color backgroundColor3 = Color(0xff242231);
-Color backgroundColor4 = Color(0xff242231);
+Color backgroundColor4 = Color(0xff252836);
+Color backgroundColor5 = Color(0xff2B2844);
 Color primaryTextColor = Color(0xffF1F0F2);
 Color secondaryTextColor = Color(0xff999999);
-Color subtitleColor = Color(0xff252836);
+Color subtitleColor = Color(0xff504F5E);
 Color transparentColor = Colors.transparent;
 Color blackColor = Color(0xff2E2E2E);
 
